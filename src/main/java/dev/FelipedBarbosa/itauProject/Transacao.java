ce60a7ffd2b3341132e0075063ef2ff6bed16e8b
Felipe Barbosa <felipe2016depaula@gmail.com>
2026-02-18 14:03:55 -0300

    feat: cria DTO Transacao conforme especificação do desafio

--- src/main/java/dev/FelipedBarbosa/itauProject/Transacao.java
+++ src/main/java/dev/FelipedBarbosa/itauProject/Transacao.java
@@ -1,3 +1,14 @@
 package dev.FelipedBarbosa.itauProject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transacao {
+    private BigDecimal valor;
+    private LocalDate dataHora;
+}
